9e9c133430822d748c4b682c426d33f6f8dd19d1
Nolan Coe <nolancoe@protonmail.com>
2025-04-23 17:33:33 -0500

    security improvements again

--- MrCoe/MrCoe/settings.py
+++ MrCoe/MrCoe/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+import logging
+import logging.config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,7 +144,7 @@ LOGGING = {
         'honeypot_file': {
             'level': 'WARNING',
             'class': 'logging.FileHandler',
-            'filename': '/var/log/django/honeypot.log',
+            'filename': os.path.join(BASE_DIR, 'honeypot.log'),
             'formatter': 'default',
         },
     },
